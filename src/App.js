56427a3f78342767b3d242782a0a169b02e1e3b0
Samuel Akinfolarin <Akinfolarin001@gmail.com>
2023-02-02 23:20:24 +0100

    progressing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Switch, Route, Redirect  } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import { Box } from '@material-ui/core';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer';
 
 import './App.css';
 
 function App() {
+
   return (
-    <div>
-      
-    </div>
+    <Box width='400px' sx={{width:{xl:'1488px'}}} m='auto'>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+    </Routes>
+
+    <Footer/>
+
+    </Box>
   );
 }
 
